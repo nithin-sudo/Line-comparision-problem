d8b9836e03673df8bf0e59c03e6f12e95e97d643
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-22 19:48:38 +0530

    Added:Line compariison class which shows Welcome message

---
+++ src/com/bridgelab/LineComparision.java
@@ -0,0 +1,7 @@
+package com.bridgelab;
+
+public class LineComparision {
+    public static void lineDistance() {
+        System.out.println("Welcome to line comparision computation problem");
+    }
+}
